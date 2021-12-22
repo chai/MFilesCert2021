df9307d6e0e8d36532a6f17ca029c9e6bc80e6f9
Chai <chaihongnge@gmail.com>
2021-12-22 17:22:46 +1100

    2.3.2 done. Starting 2.3.3

---
+++ Acme.Corporation.Storata.Chai.Nge/VaultEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using MFiles.VAF.Common;
+using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Corporation.Storata.Chai.Nge
+{
+    public partial class VaultApplication
+    {
+        [EventHandler(MFEventHandlerType.MFEventHandlerAfterCheckInChangesFinalize, ObjectType = "MF.OT.Person")]
+
+        public void CheckForName(EventHandlerEnvironment env)
+        {
+            string PersonClassName= "Person";
+            //Applied only to Person class, normally would apply via Attribute 
+            //e.g.         [EventHandler(MFEventHandlerType.MFEventHandlerAfterCheckInChangesFinalize, ObjectType = "F.OT.Person", Class = "Class Alias")]
+            //but as we're not changing anything in the vault, including adding alia
+            //we're setting up a manual check
+
+            if (env.ObjVerEx.GetPropertyText(MFBuiltInPropertyDef.MFBuiltInPropertyDefClass).Equals(PersonClassName))
+            {
+
+            }
+        }
+    }
+}
